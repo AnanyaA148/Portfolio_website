5a8bf1baf776a37f930c37f473e04e392e4c35cf
Ananya A <138643111+AnanyaA148@users.noreply.github.com>
2024-10-13 04:46:21 +0000

    Github pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ananya Agarwal",
+  title: "First Year Computer Science Student",
+  email: "an953660@ucf.edu",
+  gitHub: "AnanyaA148",
+  instagram: "",
+  linkedIn: "ananya-agarwal-w2048",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
